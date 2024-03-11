6158cc26a1ab81c90aaea073f0b501e0544c59f1
SumiKim <81948599+suri-pu-bi@users.noreply.github.com>
2024-03-11 19:31:36 +0900

    [Gold II] Title: 저울, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2437. 저울/저울.py
+++ 백준/Gold/2437. 저울/저울.py
@@ -1,21 +1,13 @@
 import sys
 n = int(sys.stdin.readline())
-weight = [0]
-weight.extend(list(map(int, sys.stdin.readline().split())))
+weight = (list(map(int, sys.stdin.readline().split())))
 weight.sort()
 
-temp = 0
-arr = []
-for i in range(len(weight)):
-    temp += weight[i]
-    arr.append(temp)
-
-flag = False
-for i in range(1, len(weight)):
-    if arr[i-1] + 1 < weight[i]:
-        print(arr[i-1] + 1)
-        flag = True
+target = 1
+for i in weight:
+    if target < i:
         break
+    target += i
 
-if flag == False:
-    print(arr[-1]+1)
+print(target)
+
