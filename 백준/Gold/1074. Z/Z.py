74b5e40a354b365341e6da8630328f87bb9c1cb9
SumiKim <81948599+suri-pu-bi@users.noreply.github.com>
2024-09-30 18:15:54 +0900

    [Gold V] Title: Z, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/1074. Z/Z.py
@@ -0,0 +1,24 @@
+import sys
+N, r, c = map(int, sys.stdin.readline().split())
+
+res = 0
+while N != 0:
+    # 4등분으로 분할
+    N -= 1
+    if r < 2**N and c < 2**N:
+        res += 0
+
+    elif r < 2**N and c >= 2**N:
+        res += (2**N) * (2**N) * 1
+        c -= (2**N)
+
+    elif r >= 2**N and c < 2**N:
+        res += (2**N) * (2**N) * 2
+        r -= (2**N)
+
+    else:
+        res += (2**N) * (2**N) * 3
+        r -= (2**N)
+        c -= (2**N)
+
+print(res)
