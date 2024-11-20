2de0514dce7519a3019a23dbb4298a3471510d92
SumiKim <81948599+suri-pu-bi@users.noreply.github.com>
2024-11-20 23:22:19 +0900

    [Silver III] Title: 1로 만들기, Time: 124 ms, Memory: 18256 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,23 +1,24 @@
 import java.io.*;
 
 public class Main {
-	static int minCnt = Integer.MAX_VALUE;
+	static int[] dp;
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
+		dp = new int[N+1];
 
-		dfs(N, 0);
-		System.out.println(minCnt);
-	}
-	// 완전탐색 DFS 해서 최솟값 계산
-	static void dfs(int n, int cnt) {
-		if (minCnt <= cnt) return;
-		if (n == 1) {
-			minCnt = Math.min(minCnt, cnt);
-		} else { // if-else가 아닌 if문으로 연결되어있으므로 여러개로 가지치기 가능
-			if(n%2 == 0) dfs(n/2, cnt+1);
-			if(n%3 == 0) dfs(n/3, cnt+1);
-			dfs(n-1, cnt+1);
+		dp[1] = 0;
+		for (int i=2; i<N+1; i++) {
+			dp[i] = dp[i-1] + 1;
+			if (i % 2 == 0 && dp[i] > dp[i/2] + 1) {
+				dp[i] = dp[i/2] + 1;
+			}
+
+			if (i % 3 == 0 && dp[i] > dp[i/3] + 1) {
+				dp[i] = dp[i/3] + 1;
+			}
 		}
+
+		System.out.println(dp[N]);
 	}
 }
